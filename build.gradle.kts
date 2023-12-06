09fde0b0b5972649f6aa047d3e043ad1aa5339f8
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2023-12-06 23:55:07 +0100

    Update Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,6 @@ version = "1.0-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
 }
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-}
-tasks.withType<Javadoc>().configureEach {
-    options.encoding = "UTF-8"
-}
 
 repositories {
     mavenCentral()
